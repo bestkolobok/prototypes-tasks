95a6246015d97f55879ea5d88e0e7c94f2c09ce5
ivanlapa <sstage44@gmail.com>
2018-03-21 06:42:36 +0200

    Add step 3

---
+++ fabricByClass.js
@@ -0,0 +1,31 @@
+(function() {
+  class Person {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+      this.family = true;
+
+      // this.getFamilyStatus = () => {
+      //   return 'My method!';
+      // }
+
+    }
+
+    getFamilyStatus() {
+      if (!this.family) {
+        return 'Free';
+      }
+
+      return 'married';
+    }
+  }
+
+
+
+
+  var ivan = new Person('Ivan', 'male');
+  console.log('New person: ', ivan);
+
+  console.log('New person familyStatus: ', ivan.getFamilyStatus());
+
+})();
